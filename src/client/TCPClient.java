79be614d2dcd36fd6c448f3e63792dcb68dfb5ee
Steve Kim <ElSteve05@gmail.com>
2014-03-06 17:19:13 -0600

    TCP Client does not need to be a thread anymore. We will now have multiple processes running a dedicated socket
    Also made changes to use ServerSocket to implement connection timeout
    feature

--- src/client/TCPClient.java
+++ src/client/TCPClient.java
@@ -4,38 +4,43 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.Socket;
+import java.net.*;
 
 /**
  * Created by prateek on 2/16/14.
  */
-public class TCPClient implements Runnable{
+public class TCPClient {
+    private final int socketTimeout = 1000;
+
     int len, port;
     String hostname, sentence, modifiedSentence;
     Socket clientSocket;
+    ServerSocket tcpSocket;
 
-    public TCPClient( int port, String hostname) {
+    public TCPClient( int port, String hostname, String message) {
         this.len = len;
         this.port = port;
         this.hostname = hostname;
-
+        this.sentence = message;
 
         try {
-            clientSocket = new Socket(hostname, port);
+            tcpSocket = new ServerSocket(port);
+            tcpSocket.setSoTimeout(socketTimeout);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    @Override
-    public void run(){
-        BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
+    public boolean connectTCP(){
+        //BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
         DataOutputStream outToServer = null;
+
         try {
+            clientSocket = tcpSocket.accept();
             // Send to Server
             outToServer = new DataOutputStream(clientSocket.getOutputStream());
             BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            sentence = inFromUser.readLine();
+            //sentence = inFromUser.readLine();
             outToServer.writeBytes(sentence + '\n');
 
             // Receive from Server
@@ -43,9 +48,13 @@ public class TCPClient implements Runnable{
             System.out.println(modifiedSentence);
             clientSocket.close();
 
+        } catch (SocketTimeoutException to) {
+            return true;
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        return false;
     }
 
 }
